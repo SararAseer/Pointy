26ba6cce162fd68be218f0797ed44ca24385f3c5
Sarar Aseer <saseer@stuy.edu>
2018-10-01 23:43:39 -0400

    i had to rework the whole thing bc i deleted the file

--- array_swap.c
+++ array_swap.c
@@ -4,23 +4,23 @@
 int main(){
   int p1[10];
   srand(time(NULL));
-  int counter=0;
+  int counter=9;
   
   while(counter--){
     p1[counter] = rand();
   }
   p1[9] = 0;
-  counter+=9;
+  counter=9;
   while(counter--){
     printf("value number: %d, in ray_one: %d \n", 9-counter, p1[9-counter]);
   }
   printf("\n");
   int p2[10];
-  int *p12 = ray_one;
-  int *p22 = ray_two;
+  int *p12 = p1;
+  int *p22 = p2;
   counter=9;
   while(counter--){
-    *(p22 + counter) = *(p12 + 9 - counter);
+    *(p22 + 9-counter) = *(p12 + counter+1);
   }
   counter=9;
   while(counter--){
